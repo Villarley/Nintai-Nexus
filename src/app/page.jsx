b4cf3308baf28b5756725dc34f00e3ca96e12efd
santiago villarreal arley <santivillarley1010@gmail.com>
2024-05-18 15:49:08 -0600

    Adding the video with VIMEO

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,21 @@
 "use client"
-import Image from 'next/image'
-import nebula from '@next/font/local'
-import kleemax from '@next/font/local'
-import { IBM_Plex_Sans } from 'next/font/google'
+import Image from "next/image"
+import nebula from "@next/font/local"
+import kleemax from "@next/font/local"
+import { IBM_Plex_Sans } from "next/font/google"
 import { Hero } from "@/components/"
 import { Navbar } from "@/components/Navbar"
-import { NintaiTitle } from '@/assets/Bgs'
+import { NintaiTitle } from "@/assets/Bgs"
 
 const Kleemax = kleemax({
-  src: '../assets/fonts/Kleemax.ttf',
-  variable: '--font-kleemax'
+  src: "../assets/fonts/Kleemax.ttf",
+  variable: "--font-kleemax"
 })
 
 const IBM_Plex = IBM_Plex_Sans({
-  subsets: ['latin'],
-  weight: '400',
-  variable: '--font-ibm'
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-ibm"
 })
 
 export default function Home() {
