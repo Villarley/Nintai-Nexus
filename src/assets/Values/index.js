05afa35945561360f9089e9b58322ac3aa48ca6f
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2024-05-27 18:13:00 -0600

    Adding Card values

---
+++ src/assets/Values/index.js
@@ -0,0 +1,10 @@
+export { default as Audacity } from "./Audacity.svg"
+export { default as Authenticity } from "./Authenticity.svg"
+export { default as Resilence } from "./Resilence.svg"
+export { default as Perseverance } from "./Perseverance.svg"
+export { default as Connection } from "./Connection.svg"
+export { default as Excellence } from "./Excellence.png"
+
+
+
+
