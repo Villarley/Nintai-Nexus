23a6e06fe23d5483ee40b1de95d22b004aaeeaab
santiago villarreal arley <santivillarley1010@gmail.com>
2024-05-31 20:30:10 -0600

    Values aded

--- src/assets/Values/index.js
+++ src/assets/Values/index.js
@@ -3,7 +3,7 @@ export { default as Authenticity } from "./Authenticity.svg"
 export { default as Resilence } from "./Resilence.svg"
 export { default as Perseverance } from "./Perseverance.svg"
 export { default as Connection } from "./Connection.svg"
-export { default as Excellence } from "./Excellence.png"
+export { default as Excellence } from "./Excellence.svg"
 
 
 
