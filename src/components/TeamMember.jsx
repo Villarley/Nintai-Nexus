e540a9cad72882ff69fb5b96f429b4c9d6a53bf3
Santiago Lobo <lobo09.slu@gmail.com>
2024-06-01 19:24:00 -0600

    Update TeamMember.jsx

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -10,7 +10,7 @@ const IBM_Plex = IBM_Plex_Sans({
 
 export default function TeamMember({ Name, Surname, Role, Img }) {
       return (
-      <div className="flex flex-col lg:flex-row gap-6 lg:gap-12 items-center justify-center p-10">
+      <div className="flex flex-col lg:flex-row gap-8 lg:gap-12 items-center justify-center p-10">
             <Image src={Img} alt={Name} className="h-72 w-56"/>
             <div className="">
                   <Kleemax><h1 className="text-4xl md:text-6xl text-Indigo">{Name}</h1><h1 className="text-3xl md:text-6xl">{Surname}</h1></Kleemax>
