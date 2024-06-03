3b6f9e7ecf70a3eb0aee2f1584153dd4b55d8a6c
santiago villarreal arley <santivillarley1010@gmail.com>
2024-06-03 10:15:32 -0600

    Almost done

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -31,9 +31,9 @@ export default function Home() {
       </Hero>
       <Sponsors/>
       <CarDetails/>
-      <AboutUs/>
       <Values/>
       <TeamCarousel/>
+      <AboutUs/>
       <Footer/>
     </>
   )
