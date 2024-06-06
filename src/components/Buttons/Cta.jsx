84cf2db9b197f69911eeae6bb779f8dd4eb9c1ec
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2024-06-05 20:17:55 -0600

    Typo fixed

--- src/components/Buttons/Cta.jsx
+++ src/components/Buttons/Cta.jsx
@@ -8,7 +8,7 @@ export default function Cta({ children, }) {
     const router = useRouter()
   
     return (
-    <button onClick={()=>router.push("/NN24")} className="text-2xl text-center p-2 rounded-xl bg-gradient-to-r from-[#a093f6] to-[#7a01f4] hover:from-Bone hover:to-Bone hover:text-Indigo transition duration-300 ease-in-out">
+    <button onClick={()=>router.push("/Car")} className="text-2xl text-center p-2 rounded-xl bg-gradient-to-r from-[#a093f6] to-[#7a01f4] hover:from-Bone hover:to-Bone hover:text-Indigo transition duration-300 ease-in-out">
       <Ibm> {children} </Ibm>
     </button>
   )
