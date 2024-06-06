e809bae1b5bbe5c18962330c3a0f751ebce88be8
Santiago Villarreal Arley <115122095+Villarley@users.noreply.github.com>
2024-06-05 20:27:54 -0600

    Adding text

--- src/app/car/page.jsx
+++ src/app/car/page.jsx
@@ -1,6 +1,7 @@
 // src/app/car/page.jsx
 "use client"
 import React from 'react'
+import { Nebula } from "@/components/Text";
 import dynamic from 'next/dynamic'
 
 const CarCanvas = dynamic(() => import('@/components/Car/Carcanvas'), { ssr: false })
@@ -9,6 +10,7 @@ export default function Page() {
   return (
     <>
     <div className="h-screen bg-white">
+      <h1 className="text-3xl text-black text-center"><Nebula>Presenting the NN24</Nebula></h1>
       <CarCanvas />
     </div>
     </>
