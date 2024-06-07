d23728576694b590101a57013ee10deb69e586cf
santiago villarreal arley <santivillarley1010@gmail.com>
2024-06-07 17:49:21 -0600

    Last fix

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,7 +11,7 @@ export default function AboutUs() {
       <Image draggable={false}  src={Isotype} className="  w-60 lg:w-96" alt=""/>
       <div className=" lg:w-6/12 flex flex-col justify-center lg:self-start lg:gap-5 mt-12">
             <Nebula> <h1 className="text-Indigo text-5xl text-center">About Us</h1></Nebula>
-            <p className="text-black text-justify text-md lg:text-lg">NINTAI NEXUS is an F1 in Schools team with Japanese disruptive and futuristic style, reflecting innovation in every aspect of their creation. As a team we develop the most ingenious and creative ideas for our 3 different departements: Brand Management, Innovation Development and Car Engineers were we work together to create the NN24. Guided and representent by our japanese and salesians values, we bring the best work possible out to shine made with the effort of every team member joined to show the Ultimate Team.</p>
+            <p className="text-black text-justify text-lg lg:text-xl">NINTAI NEXUS is an F1 in Schools team with Japanese disruptive and futuristic style, reflecting innovation in every aspect of their creation. As a team we develop the most ingenious and creative ideas for our 3 different departements: Brand Management, Innovation Development and Car Engineers were we work together to create the NN24. Guided and representent by our japanese and salesians values, we bring the best work possible out to shine made with the effort of every team member joined to show the Ultimate Team.</p>
       </div>
     </div>
   );
